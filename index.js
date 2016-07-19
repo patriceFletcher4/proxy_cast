db7374acdbffc801ab8bb4fcfdf0301d69ce5e09
PFletcher <PFletcher>
2016-07-18 20:18:53 -0400

    added data to project

--- index.js
+++ index.js
@@ -4,16 +4,32 @@ var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var server = express();
+var $http = require('axios');
 
 var port = process.env.PORT || 8080;
+var apiKey = require('./config').apiKey;
+var baseUrl = 'https://api.forecast.io/forecast/';
+
 
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({extended: true}));
 server.use(cors());
 
 //test route
-server.get('/', function(req, res){
-    res.send('hello');
+server.get('/forecast/hourly/:lat,:lon', function(req, res){
+     $http.get(baseUrl + apiKey + '/'+req.params.lat+','+req.params.lon)
+          .then(function(response){
+            var resOBj = {
+              latitude: response.data.latitude,
+              longitude: response.data.longitude,
+              hourly: response.data.hourly
+            };
+            res.json(resOBj);
+          })
+          .catch(function(error){
+            console.log(error);
+            res.send('badddd!!!!')
+          });
 });
 
 server.listen(port, function(){
