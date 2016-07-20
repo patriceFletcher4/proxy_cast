3d427819107328142394db641be4c78aabe08840
PFletcher <PFletcher>
2016-07-20 18:53:31 -0400

    created daily route

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ server.get('/forecast/hourly/:lat,:lon', function(req, res){
             res.status(500).json({
               msg: error
             });
-      
+        });
+    });
 
 server.get('/forecast/minutely/:lat,:lon', function(req, res){
       $http.get(baseUrl + apiKey + '/'+req.params.lat+','+req.params.lon)
@@ -46,7 +47,41 @@ server.get('/forecast/minutely/:lat,:lon', function(req, res){
               res.status(500).json({
                 msg: error
               });
-          });
+
+            });
+    });
+server.get('/forecast/daily/:lat,:lon', function(req, res){
+  $http.get(baseUrl + apiKey + '/' + req.params.lat + ',' + req.params.lon)
+      .then(function(response){
+        var overSummary = response.data.daily.summary;
+        var overIcon = response.data.daily.icon;
+        var dailyData = response.data.daily.data;
+        var dailyArr = [];
+        for(var i = 0; i < dailyData.length; i += 1){
+          var o = {
+          tempMax: dailyData[i].tempratureMax,
+          tempMin: dailyData[i].tempratureMin,
+          humidity: dailyData[i].humidity,
+          icon: dailyData[i].icon,
+          precipProb: dailyData[i].precipProbability
+        };
+        dailyArr.push(0);
+      }
+
+      var resOBj = {
+        latitude: response.data.latitude,
+        longitude: response.data.longitude,
+        summary: overSummary,
+        icon: overIcon,
+        daily: dailyArr
+      };
+        res.status(200).json(resOBj);
+      })
+      .catch(function(error){
+        res.status(500).json({
+          msg: error
+        });
+      });
 
 });
 
